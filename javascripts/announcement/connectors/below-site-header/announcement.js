e3ef0d4389ed3e3e4df4e4392dfc44f1667b725c
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-07-19 11:44:18 -0700

    separate dots from the carousel body

--- javascripts/announcement/connectors/below-site-header/announcement.js
+++ javascripts/announcement/connectors/below-site-header/announcement.js
@@ -1,23 +1,25 @@
-import { withPluginApi } from "discourse/lib/plugin-api";
+import {
+  withPluginApi
+} from "discourse/lib/plugin-api";
 
 export default {
   setupComponent(attrs, component) {
-    const track = document.querySelector('.carousel-track');
-    const slides = Array.from(track.children);
-    const prevBtn = document.querySelector('.carousel-btn-left');
-    const nextBtn = document.querySelector('.carousel-btn-right');
-    const dotsNav = document.querySelector('.dots');
-    const dots = Array.from(dotsNav.children);
 
-    const slideWidth = slides[0].getBoundingClientRect().width;
+    withPluginApi("0.11", (api) => {
+      api.onPageChange(() => {
+        const track = document.querySelector('.carousel-track');
+        const slides = Array.from(track.children);
+        const prevBtn = document.querySelector('.carousel-btn-left');
+        const nextBtn = document.querySelector('.carousel-btn-right');
+        const dotsNav = document.querySelector('.dots');
+        const dots = Array.from(dotsNav.children);
 
-      withPluginApi("0.11", (api) => {
-        api.onPageChange(() => {
-          slides.forEach((slide, i) => {
-            slide.style.left = `${slideWidth * index}px`;
-          });
-          
+        const slideWidth = slides[0].getBoundingClientRect().width;
+        slides.forEach((slide, i) => {
+          slide.style.left = `${slideWidth * index}px`;
         });
+
       });
+    });
   },
 };
