b1ecd148dd67f58fbce5d37fbcc755e1a20e7a03
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-07-19 12:07:17 -0700

    separate dots from the carousel body

--- javascripts/announcement/connectors/below-site-header/announcement.js
+++ javascripts/announcement/connectors/below-site-header/announcement.js
@@ -13,18 +13,32 @@ export default {
         const nextBtn = document.querySelector('.carousel-btn-right');
         const dotsNav = document.querySelector('.dots');
         const dots = Array.from(dotsNav.children);
+        const flagClassName = 'currentSlide';
 
         const slideWidth = slides[0].getBoundingClientRect().width;
         slides.forEach((slide, i) => {
           slide.style.left = `${slideWidth * i}px`;
         });
+
+        const moveToSide = (track, currentSlide, targetSlide) => {
+          track.style.transform = `translateX(-${targetSlide.style.left})`;
+          currentSlide.classList.remove(flagClassName);
+          targetSlide.classList.add(flagClassName);
+        }
+
+        prevBtn.addEventListener('click', (e) => {
+          const currentSlide = track.querySelector(`.${flagClassName}`);
+          const prevSlide = currentSlide.previousElementSibling;
+          moveToSide(track, currentSlide, prevSlide);
+        });
+
         nextBtn.addEventListener('click', (e) => {
-          const currentSlide = track.querySelector('.currentSlide');
+          const currentSlide = track.querySelector(`.${flagClassName}`);
           const nextSlide = currentSlide.nextElementSibling;
-          // move to the next slide
-          const amountToMove = nextSlide.style.left;
-          track.style.transform = `translateX(-${amountToMove})`;
-        })
+          moveToSide(track, currentSlide, nextSlide);
+        });
+
+        // dotsNav.
 
       });
     });
