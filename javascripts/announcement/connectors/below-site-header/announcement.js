719c1bfe8f1546d0f28ad8e95d21ec0882919ed2
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-07-20 13:56:10 -0700

    adjust the font size of all the elements

--- javascripts/announcement/connectors/below-site-header/announcement.js
+++ javascripts/announcement/connectors/below-site-header/announcement.js
@@ -7,93 +7,98 @@ export default {
 
     withPluginApi("0.11", (api) => {
       api.onPageChange(() => {
-        
-          const track = document.querySelector('.carousel-track');
-          const slides = track? Array.from(track.children) : null;
-          const prevBtn = document.querySelector('.carousel-btn-left');
-          const nextBtn = document.querySelector('.carousel-btn-right');
-          const dotsNav = document.querySelector('.dots');
-          const dots = dotsNav? Array.from(dotsNav.children) : null;
-          const flagClassName = 'currentSlide';
+        const eventBanner = document.querySelector('.event-banner');
+        const track = document.querySelector('.carousel-track');
+        const slides = track ? Array.from(track.children) : null;
+        const prevBtn = document.querySelector('.carousel-btn-left');
+        const nextBtn = document.querySelector('.carousel-btn-right');
+        const dotsNav = document.querySelector('.dots');
+        const dots = dotsNav ? Array.from(dotsNav.children) : null;
+        const flagClassName = 'currentSlide';
 
-          let currentIndex = 0;
+        let currentIndex = 0;
 
-          const moveIndex = offset => {
-            const targetIndex = currentIndex + offset;
-            if (targetIndex < 0) {
-              currentIndex = slides.length - 1;
-            } else if (targetIndex > slides.length - 1) {
-              currentIndex = 0;
-            } else {
-              currentIndex = targetIndex;
-            }
+        const moveIndex = offset => {
+          const targetIndex = currentIndex + offset;
+          if (targetIndex < 0) {
+            currentIndex = slides.length - 1;
+          } else if (targetIndex > slides.length - 1) {
+            currentIndex = 0;
+          } else {
+            currentIndex = targetIndex;
           }
-          let slideWidth;
-          const adjustWidth = () => {
-            slideWidth = slides[0].getBoundingClientRect().width;  
-          }
+        }
+        let slideWidth;
+        const adjustWidthAndHeight = () => {
+          const sizeInfo = slides[0].getBoundingClientRect();
+          slideWidth = sizeInfo.width;
+          slideHeight = sizeInfo.height;
 
-          if (dots && slides && prevBtn && nextBtn &&dotsNav) {
-          adjustWidth();
           slides.forEach((slide, i) => {
             slide.style.left = `${slideWidth * i}px`;
           });
+          eventBanner.style.height = `${slideHeight + 44.8}px`
+        }
+
+        if (dots && slides && prevBtn && nextBtn && dotsNav) {
+          adjustWidthAndHeight();
+
           // will come back and change these later
-          if (dots && slides && prevBtn && nextBtn &&dotsNav) {
-          window.addEventListener('resize', e => {
-            adjustWidth();
-          });
+          if (dots && slides && prevBtn && nextBtn && dotsNav) {
+            window.addEventListener('resize', e => {
+              adjustWidthAndHeight();
+            });
 
-          
 
-          const moveToSide = (track, currentSlide, targetSlide) => {
+
+            const moveToSide = (track, currentSlide, targetSlide) => {
               track.style.transform = `translateX(-${targetSlide.style.left})`;
               currentSlide.classList.remove(flagClassName);
               targetSlide.classList.add(flagClassName);
-          }
-  
-          const updateDots = (currentDot, targetDot) => {
-            currentDot.classList.remove('currentDot');
-            targetDot.classList.add('currentDot');
-          }
-  
-          prevBtn.addEventListener('click', (e) => {
-            const currentSlide = track.querySelector(`.${flagClassName}`);
-            moveIndex(-1);
-            const prevSlide = slides[currentIndex];
-            const currentDot = dotsNav.querySelector(`.currentDot`);
-            const prevDot = dots[currentIndex];
+            }
 
-            moveToSide(track, currentSlide, prevSlide);
-            updateDots(currentDot, prevDot);
-          });
-  
-          nextBtn.addEventListener('click', (e) => {
-            const currentSlide = track.querySelector(`.${flagClassName}`);
-            moveIndex(1);
-            const nextSlide = slides[currentIndex];
-            const currentDot = dotsNav.querySelector(`.currentDot`);
-            const nextDot = dots[currentIndex];
-            moveToSide(track, currentSlide, nextSlide);
-            updateDots(currentDot, nextDot);
-          });
-  
-          dotsNav.addEventListener('click', e => {
-            const targetDot = e.target.closest('li');
-            if (!targetDot) return;
-  
-            const currentSlide = track.querySelector(`.${flagClassName}`);
-            const currentDot = dotsNav.querySelector(`.currentDot`);
-            const targetIndex = dots.findIndex(dot => dot === targetDot);
-            currentIndex = targetIndex;
-            const targetSlide = slides[targetIndex];
-  
-            moveToSide(track, currentSlide, targetSlide);
-            updateDots(currentDot, targetDot);
-          });
+            const updateDots = (currentDot, targetDot) => {
+              currentDot.classList.remove('currentDot');
+              targetDot.classList.add('currentDot');
+            }
+
+            prevBtn.addEventListener('click', (e) => {
+              const currentSlide = track.querySelector(`.${flagClassName}`);
+              moveIndex(-1);
+              const prevSlide = slides[currentIndex];
+              const currentDot = dotsNav.querySelector(`.currentDot`);
+              const prevDot = dots[currentIndex];
+
+              moveToSide(track, currentSlide, prevSlide);
+              updateDots(currentDot, prevDot);
+            });
+
+            nextBtn.addEventListener('click', (e) => {
+              const currentSlide = track.querySelector(`.${flagClassName}`);
+              moveIndex(1);
+              const nextSlide = slides[currentIndex];
+              const currentDot = dotsNav.querySelector(`.currentDot`);
+              const nextDot = dots[currentIndex];
+              moveToSide(track, currentSlide, nextSlide);
+              updateDots(currentDot, nextDot);
+            });
+
+            dotsNav.addEventListener('click', e => {
+              const targetDot = e.target.closest('li');
+              if (!targetDot) return;
+
+              const currentSlide = track.querySelector(`.${flagClassName}`);
+              const currentDot = dotsNav.querySelector(`.currentDot`);
+              const targetIndex = dots.findIndex(dot => dot === targetDot);
+              currentIndex = targetIndex;
+              const targetSlide = slides[targetIndex];
+
+              moveToSide(track, currentSlide, targetSlide);
+              updateDots(currentDot, targetDot);
+            });
           }
         }
-        
+
       });
     });
   },
